dc3e7fc0c164b8ea266771c704e607c793ba797e
Tyler Guillen <tyguillen@gmail.com>
2016-09-03 13:32:06 -0400

    add top bar authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Myflix::Application.routes.draw do
   get '/register', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   get '/home', to: 'videos#index'
 
   resources :videos, only: [:index, :show] do
