3d3bf5282f4c99b5aae3d53f54f88aa7cca59bd0
Tyler Guillen <tyguillen@gmail.com>
2016-09-03 12:21:17 -0400

    login working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def access_denied(msg = "You aren't allowed to do that.")
+    flash[:error] = msg
+    redirect_to root_path
+  end
+
+  def require_user
+    access_denied('You must be logged in to do that.') unless logged_in?
+  end
+
+  def require_logged_out
+    access_denied('You must be logged out to do that.') if logged_in?
+  end
+
+  def require_logged_in_as(user)
+    access_denied unless current_user == user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,26 @@
 class SessionsController < ApplicationController
+  before_action :require_logged_out, only: [:new, :create]
+  before_action :require_user, only: [:destroy]
+
   def new
   end
 
   def create
-    user = User.find_by username:(params[:username])
+    user = User.find_by email: (params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "You're logged in!"
-      redirect_to root_path
+      flash[:success] = "You're logged in!"
+      redirect_to home_path
     else
-      flash[:error] = "There's something wrong with your username or password."
+      flash[:error] = "There's something wrong with your email or password."
       redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = 'You have successfully logged out.'
+    flash[:success] = 'You have successfully logged out.'
     redirect_to root_path
   end
 end
