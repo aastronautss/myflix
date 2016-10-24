11a7103e9b80185fc9747c443a5db7d733a3c5d7
Tyler Guillen <tyguillen@gmail.com>
2016-10-24 10:58:51 -0400

    add infrastructure for adding/watching the actual video files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024141104) do
+ActiveRecord::Schema.define(version: 20161024145137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20161024141104) do
     t.integer  "category_id"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "watch_url"
   end
 
 end
