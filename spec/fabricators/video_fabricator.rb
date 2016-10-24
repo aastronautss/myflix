eee09cf0aa8a057d228628e661248aeb4e50779d
Tyler Guillen <tyguillen@gmail.com>
2016-10-24 15:02:37 -0400

    oops this turned into the payment acceptance branch

--- spec/fabricators/video_fabricator.rb
+++ spec/fabricators/video_fabricator.rb
@@ -3,6 +3,7 @@ Fabricator :video do
   description { Faker::Lorem.paragraph }
   remote_small_cover_url { Faker::Placeholdit.image '166x236' }
   remote_large_cover_url { Faker::Placeholdit.image '665x375' }
+  watch_url 'https://www.youtube.com/watch?v=J5KkKdGDs5Y'
   reviews(rand: 15) { Fabricate(:review, video: nil) }
   category do
     if Category.any?
